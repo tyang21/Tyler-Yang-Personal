ad4b9e55d4b69a6f15217cfb329d00a5fff52fd9
Tyler Yang <tsyang59@gmail.com>
2026-02-25 07:32:15 -0800

    Removing google fonts for vercel

--- app/CS180/layout.tsx
+++ app/CS180/layout.tsx
@@ -1,13 +1,6 @@
 import '../globals.css'
 import { Metadata } from "next";
-import Providers from "../../components/Themeprovider";
-import Themechanger from "../../components/Themechanger";
 import Navbar from '@/components/navbar';
-import Footer from "../../components/Footer";
-import { ThemeProvider } from "next-themes"
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Tyler Yang",
@@ -20,9 +13,9 @@ export default function About ({
   children: React.ReactNode
 }) {
   return (
-    <div  className = "text-[var(--text-main)]">
-        <Navbar/>
-        {children}
+    <div className="text-[var(--text-main)]">
+      <Navbar/>
+      {children}
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
-
 import './globals.css'
 import { Metadata } from "next";
 import Providers from "../components/Themeprovider";
 import Themechanger from "../components/Themechanger";
-import Navbar from '../components/navbar';
 import Footer from "../components/Footer";
-import { ThemeProvider } from "next-themes"
-import { Inter } from 'next/font/google'
-import { Crimson_Pro } from 'next/font/google'
-import { Montserrat } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
-
-const crimson = Crimson_Pro({ subsets: ['latin'] })
-const montserrat = Montserrat({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "Tyler Yang",
   description: "Manually Created",
@@ -26,8 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
-        <div  className = "text-[var(--text-main)]">
+      <body>
+        <div className="text-[var(--text-main)]">
           <Providers>
             <div className="overflow-x-hidden">
               <Themechanger />
@@ -36,7 +26,6 @@ export default function RootLayout({
           {children}
           <Footer />
         </div>
-        
       </body>
     </html>
   )
